25159d065e33e3a572a24704661bf9211b66b146
olaniyi oladimeji <88807990+olaniyi93@users.noreply.github.com>
2022-07-30 22:02:16 +0000

    Day one of python boot camp practice

--- main.py
+++ main.py
@@ -2,3 +2,14 @@
 print ("Day 1 - String Manipulation")
 print("String Concatenation is done with the '+' sign.")
 print("New lines be created with a backlash and n.")
+print ("Hello " +( input("What is your name? ")))
+name = input('What is the game ')
+count = len(name)
+print(count)
+a = input("a: ")
+b = input("b: ")
+c = a
+a = b
+b = c
+print ("a: "+ a)
+print ("b: "+ b)
