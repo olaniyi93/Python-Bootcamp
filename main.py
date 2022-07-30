8b3fd0f8a54f9eed9dc53c1857acead33e6f571a
Ibrahim Oladimeji <>
2022-07-30 20:49:43 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,4 @@
+# Write your code below this line 👇
+print ("Day 1 - String Manipulation")
+print("String Concatenation is done with the '+' sign.")
+print("New lines be created with a backlash and n.")
